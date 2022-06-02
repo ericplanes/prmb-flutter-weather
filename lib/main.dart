4985fa52187e6b051807c3685589b3dcbcd65e97
Eric Planes <eric.planes@students.salle.url.edu>
2022-06-02 19:33:06 +0200

    Feature: Login UI done.

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,114 @@ Future main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "MyApp Title",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            ), // TextStyle
+          ), //Text
+          const Text(
+            "Login to Your App",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44.0,
+              fontWeight: FontWeight.bold,
+            ), // TextStyle
+          ), // Text
+          const SizedBox(
+            height: 44.0,
+          ), // SizedBox
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "User Email",
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
+            ), // InputDecoration
+          ), // TextField
+          const SizedBox(
+            height: 26.0,
+          ), // SizedBox
+          const TextField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: "User Password",
+              prefixIcon: Icon(Icons.lock, color: Colors.black),
+            ), // Input Decoration
+          ), // TextField
+          const SizedBox(
+            height: 12.0,
+          ), // SizedBox
+          const Text(
+            "Don't Remember your Password?",
+            style: TextStyle(color: Colors.blue),
+          ), //Text
+          const SizedBox(
+            height: 88.0,
+          ), // SizedBox
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ), // RoundedRectangleBorder
+              onPressed: () {},
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ), // TextStyle
+              ), // Text
+            ), // RawMaterialButton
+          ), // Container
+        ],
+      ), // Column
+    ); // Padding
   }
 }
 
